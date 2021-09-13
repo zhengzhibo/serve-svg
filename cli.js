e7c0111078e45327cbdfaaf778b84a2109c20b58
Alex Zheng <zhibo90@gmail.com>
2021-09-13 23:31:05 +0800

    param process

--- cli.js
+++ cli.js
@@ -5,16 +5,41 @@ const path = require('path');
 const Koa = require('koa');
 const Mustache = require('mustache');
 
+const commander  = require('commander');
+const program = new commander.Command();
 const open = require('open');
 const glob = require('glob');
+const {version} = require('./package.json');
 
 const app = new Koa();
-
 app.use(require('koa-static')(process.cwd()));
+
+function myParseInt(value, dummyPrevious) {
+  const parsedValue = parseInt(value, 10);
+  if (isNaN(parsedValue)) {
+    throw new commander.InvalidArgumentError('Not a number.');
+  }
+  return parsedValue;
+}
+
+program.version(version);
+
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('-r, --recursive', 'serve file recursive')
+  .option('-g, --glob <string>', 'use glob pattern', )
+  .option('-p, --port <number>', 'specify custom port', myParseInt, 3000);
+
+program.parse(process.argv);
+
+const options = program.opts();
+if (options.debug) console.log(options);
+
+const pattern = options.glob || (options.recursive ? '**/*.svg' : '*.svg');
 
 function getSvgFiles() {
   return new Promise((resolve, reject) => {
-    glob('**/*.svg', function (err, files) {
+    glob(pattern, function (err, files) {
       if (err) {
         reject(err);
       } else {
@@ -39,6 +64,6 @@ function render(pathname, data) {
   return Mustache.render(template, data);
 }
 
-app.listen(3000);
-console.log('serve-svg running: http://localhost:3000');
-!process.env.NODE_ENV && open('http://localhost:3000');
+app.listen(options.port);
+console.log(`serve-svg running: http://localhost:${options.port}`);
+!process.env.NODE_ENV && open(`http://localhost:${options.port}`);
